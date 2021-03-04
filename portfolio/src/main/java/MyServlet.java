74f8ba1610af51aac26a4eb584bfb8c000defed4
MathewLG <mathewlopezg@gmail.com>
2021-03-04 00:50:51 +0000

    Add Servlet and JS script to fetch greeting from index

--- portfolio/src/main/java/MyServlet.java
+++ portfolio/src/main/java/MyServlet.java
@@ -7,14 +7,9 @@ import javax.servlet.http.HttpServletResponse;
 /** Servlet that returns HTML that contains the page view count. */
 @WebServlet("/hi")
 public class MyServlet extends HttpServlet {
-
-  private int pageViews = 0;
-
   @Override
   public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
-    pageViews++;
-
     response.setContentType("text/html;");
-    response.getWriter().println("<h1>Hello,friend!</h1>");
+    response.getWriter().println("Hello,friend!");
   }
 }
--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -34,3 +34,9 @@ function addRandomQuote() {
   const greetingContainer = document.getElementById('quote-container');
   greetingContainer.innerText = quote;
 }
+async function greeting(){
+    const responseFromServer = await fetch('/hi');
+    const textFromResponse = await responseFromServer.text();
+    const greetingContainer = document.getElementById('greeting-container');
+    greetingContainer.innerText = textFromResponse;
+}
