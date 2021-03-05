555688338feb607409db0be89d5c52b1b2dbaa98
MathewLG <mathewlopezg@gmail.com>
2021-03-05 15:19:05 +0000

    Add ArrayList with greetings and JSON parse to Servlet

--- portfolio/src/main/java/MyServlet.java
+++ portfolio/src/main/java/MyServlet.java
@@ -3,13 +3,18 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.util.*;
+import com.google.gson.Gson;
 
 /** Servlet that returns HTML that contains the page view count. */
 @WebServlet("/hi")
 public class MyServlet extends HttpServlet {
+    private ArrayList<String> messages = new ArrayList<String>(Arrays.asList("Hola", "Hello", "Bonjour", "Privet", "Hei"));
   @Override
-  public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
-    response.setContentType("text/html;");
-    response.getWriter().println("Hello,friend!");
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        response.setContentType("text/html;");
+        Gson gson = new Gson();
+        String json = gson.toJson(messages);
+        response.getWriter().println(json);
   }
 }
